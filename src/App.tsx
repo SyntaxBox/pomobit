a23c77baf710bdb61a577ce7c121f50765f6e885
Abdelhamid Boudjit <qhamid@proton.me>
2024-08-27 11:04:43 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { useUI } from "../hooks";
+
 function App() {
+  const { workPallet, breakPallet } = useUI();
+
   return (
-    <>
+    <div
+      className="h-screen"
+      style={{
+        background: breakPallet.background,
+      }}
+    >
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    </div>
   );
 }
 

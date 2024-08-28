6836484d18ce4e7681b98232219bae47c21472f7
Abdelhamid Boudjit <qhamid@proton.me>
2024-08-28 14:28:34 +0100

    add linear background style

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import { PomodoroClock } from "./components/pomodoro-clock";
 import { useUI } from "./hooks";
 
 function App() {
   const { workPallet, breakPallet } = useUI();
-
+  const background = `linear-gradient( ${workPallet.background} , ${breakPallet.background}`;
   return (
     <div
       className="h-screen"
       style={{
-        background: breakPallet.background,
+        background,
       }}
     >
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <PomodoroClock />
     </div>
   );
 }

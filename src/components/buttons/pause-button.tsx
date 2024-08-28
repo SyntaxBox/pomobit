c77d6a01ea537a4b89980d8813804b788dfa7ab3
Abdelhamid Boudjit <qhamid@proton.me>
2024-08-28 15:11:01 +0100

    fix types and optionals

--- src/components/buttons/pause-button.tsx
+++ src/components/buttons/pause-button.tsx
@@ -4,24 +4,21 @@ import { Button } from "../../ui/button";
 import { useUI } from "../../hooks";
 import { Triangle, Pause } from "lucide-react";
 
-interface PauseButtonProps
-  extends Omit<HTMLAttributes<HTMLButtonElement>, "children" | "onClick"> {
-  onPause: () => void;
-  onResume: () => void;
-  pallet?: ColorUtils.ColorPallet;
-}
-
 export function PauseButton({
   onPause,
   onResume,
   pallet,
   ...rest
-}: PauseButtonProps) {
+}: Omit<HTMLAttributes<HTMLButtonElement>, "children" | "onClick"> & {
+  onPause?: () => void;
+  onResume?: () => void;
+  pallet?: ColorUtils.ColorPallet;
+}) {
   const { workPallet } = useUI();
   const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
-    isPaused ? onPause() : onResume();
+    isPaused ? onPause && onPause() : onResume && onResume();
   }, [isPaused, onPause, onResume]);
 
   const handleClick = useCallback(() => {

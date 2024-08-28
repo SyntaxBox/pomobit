c271a22e91f453dece6fa939de7850137756b465
Abdelhamid Boudjit <qhamid@proton.me>
2024-08-28 14:27:52 +0100

    create pomodoro clock component

--- src/components/pomodoro-clock.tsx
+++ src/components/pomodoro-clock.tsx
@@ -1,10 +1,12 @@
+import { useUI } from "../hooks";
 import { Clock } from "../ui";
 
 export function PomodoroClock() {
+  const { workPallet, breakPallet } = useUI();
   const time = {
     min: 24,
     sec: 4,
   };
 
-  return <Clock {...time} />;
+  return <Clock {...time} pallet={breakPallet} />;
 }

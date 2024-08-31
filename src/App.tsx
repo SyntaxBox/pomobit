fbccc673e3c9860eb0eb1e247ef69f6ab6ff71fe
Abdelhamid Boudjit <qhamid@proton.me>
2024-08-31 18:23:53 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import { PauseButton, ResetButton } from "./components";
 import { PomodoroClock } from "./components/pomodoro-clock";
-import { useUI } from "./hooks";
+import { usePomodoro, useUI } from "./hooks";
+import { usePomodoroStore } from "./store";
 
 function App() {
   const { workPallet, breakPallet } = useUI();
-  const background = `linear-gradient( ${workPallet.background} , ${breakPallet.background}`;
+  const { getGradientPercentage } = usePomodoro();
+  const { startTimer, pauseTimer, resetTimer, isBreak, isRunning } =
+    usePomodoroStore();
+
+  const percentage = getGradientPercentage();
+  const transitionWidth = 20; // Adjust this value to control the smoothness
+
+  const background = isBreak
+    ? `linear-gradient( 
+      ${breakPallet.background} ${percentage - transitionWidth / 2}%, 
+      ${workPallet.background} ${percentage + transitionWidth / 2}%)`
+    : `linear-gradient( 
+      ${workPallet.background} ${percentage - transitionWidth / 2}%, 
+      ${breakPallet.background} ${percentage + transitionWidth / 2}%)`;
   return (
     <div
       className="h-screen"
@@ -14,8 +28,12 @@ function App() {
     >
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <PomodoroClock />
-      <PauseButton />
-      <ResetButton />
+      <PauseButton
+        onPause={pauseTimer}
+        onResume={startTimer}
+        reset={!isRunning}
+      />
+      {!isBreak && <ResetButton onClick={resetTimer} />}
     </div>
   );
 }

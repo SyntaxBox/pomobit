6dfa9121f42222c9d98ef7dd0384b650d88445ef
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-01 19:40:37 +0100

    Update cn.ts

--- src/lib/utils/cn.ts
+++ src/lib/utils/cn.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const formatClassName = (names: unknown[]) =>
+  names.filter(Boolean).join(" ");

404f3b40cce527cfb857b2646443a78ed6fc07ef
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-01 16:42:57 +0100

    Update useUI.tsx

--- src/hooks/useUI.tsx
+++ src/hooks/useUI.tsx
@@ -1,10 +1,12 @@
 import { ColorUtils } from "../lib/utils";
+import { usePomodoroStore } from "../store";
 import useLocalStorage from "./useLocalStorage";
 
 export const DEFAULT_WORK = "#ee000e";
 export const DEFAULT_BREAK = "#00e030";
 
 export function useUI() {
+  const { isBreak } = usePomodoroStore();
   const [workColor, setWorkColor] = useLocalStorage("work-color", DEFAULT_WORK);
   const [breakColor, setBreakColor] = useLocalStorage(
     "break-color",
@@ -18,12 +20,16 @@ export function useUI() {
     setter(color);
   };
 
+  const workPallet = ColorUtils.generateColorPalette(workColor);
+  const breakPallet = ColorUtils.generateColorPalette(breakColor);
+
   return {
     workColor,
     breakColor,
     updateWorkColor: updateColor(setWorkColor),
     updateBreakColor: updateColor(setBreakColor),
-    workPallet: ColorUtils.generateColorPalette(workColor),
-    breakPallet: ColorUtils.generateColorPalette(breakColor),
+    workPallet,
+    breakPallet,
+    currentPallet: isBreak ? breakPallet : workPallet,
   };
 }

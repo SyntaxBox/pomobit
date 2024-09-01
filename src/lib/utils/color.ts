660c9604fd96edba4a23f07bb56f50181b7530a8
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-01 19:40:40 +0100

    Update color.ts

--- src/lib/utils/color.ts
+++ src/lib/utils/color.ts
@@ -1,4 +1,25 @@
 export type ColorPallet = ReturnType<typeof generateColorPalette>;
+
+export interface HsvColor {
+  h: number;
+  s: number;
+  v: number;
+}
+
+export interface HsvaColor extends HsvColor {
+  a: number;
+}
+
+export interface HslColor {
+  h: number;
+  s: number;
+  l: number;
+}
+
+export interface HslaColor extends HslColor {
+  a: number;
+}
+
 export function generateColorPalette(hexColor: string) {
   // Convert hex to RGB
   const rgb = hexToRgb(hexColor);
@@ -117,3 +138,31 @@ export function isHexColor(color: string): boolean {
 
   return hexColorRegex.test(color);
 }
+
+export function clamp(number: number, min = 0, max = 1) {
+  return number > max ? max : number < min ? min : number;
+}
+
+export function round(number: number, digits = 0, base = Math.pow(10, digits)) {
+  return Math.round(base * number) / base;
+}
+
+export const hsvaToHsla = ({ h, s, v, a }: HsvaColor): HslaColor => {
+  const hh = ((200 - s) * v) / 100;
+
+  return {
+    h: round(h),
+    s: round(
+      hh > 0 && hh < 200
+        ? ((s * v) / 100 / (hh <= 100 ? hh : 200 - hh)) * 100
+        : 0,
+    ),
+    l: round(hh / 2),
+    a: round(a, 2),
+  };
+};
+
+export const hsvaToHslString = (hsva: HsvaColor): string => {
+  const { h, s, l } = hsvaToHsla(hsva);
+  return `hsl(${h}, ${s}%, ${l}%)`;
+};

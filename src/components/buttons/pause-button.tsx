88bcec349a3543f5717e8ea830039b32afd226dd
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-01 18:34:10 +0100

    Update pause-button.tsx

--- src/components/buttons/pause-button.tsx
+++ src/components/buttons/pause-button.tsx
@@ -1,6 +1,6 @@
 import { HTMLAttributes, useEffect, useState, useCallback } from "react";
 import { ColorUtils } from "../../lib/utils";
-import { Button } from "../../ui/button";
+import { Button } from "../../ui";
 import { useUI } from "../../hooks";
 import { Triangle, Pause } from "lucide-react";
 

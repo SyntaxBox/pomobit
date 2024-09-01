34710008bdde3c83830ee9f5c6300f698b2f614a
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-01 16:42:59 +0100

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,27 @@
+import { Link } from "react-router-dom";
+import { useUI } from "../hooks";
+
 export function Navbar() {
-  return <div className="h-12">Hello world</div>;
+  const { currentPallet } = useUI();
+  return (
+    <div className="h-14 py-2 px-4 flex justify-between">
+      <Link to="/">
+        <img src="/icon.svg" alt="logo icon" className="w-24 h-24 -mt-5" />
+      </Link>
+
+      <div
+        style={{
+          color: currentPallet.text1,
+        }}
+        className="flex items-center gap-3 text-lg"
+      >
+        <Link to="/settings" className="hover:underline">
+          Settings
+        </Link>
+        <Link to="/analytics" className="hover:underline">
+          Analytics
+        </Link>
+      </div>
+    </div>
+  );
 }

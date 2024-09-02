d85e1f16170b1f2295a972a39c5258ccca51c28e
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-02 15:43:33 +0100

    Update useUI.tsx

--- src/hooks/useUI.tsx
+++ src/hooks/useUI.tsx
@@ -1,35 +1,18 @@
 import { ColorUtils } from "../lib/utils";
 import { usePomodoroStore } from "../store";
-import useLocalStorage from "./useLocalStorage";
-
-export const DEFAULT_WORK = "#ee000e";
-export const DEFAULT_BREAK = "#00e030";
+import { UseSettings } from "./useSettings";
 
 export function useUI() {
   const { isBreak } = usePomodoroStore();
-  const [workColor, setWorkColor] = useLocalStorage("work-color", DEFAULT_WORK);
-  const [breakColor, setBreakColor] = useLocalStorage(
-    "break-color",
-    DEFAULT_BREAK,
-  );
-
-  const updateColor = (setter: typeof setWorkColor) => (color: string) => {
-    if (!ColorUtils.isHexColor(color)) {
-      throw new Error("A color must be a valid Hex Color");
-    }
-    setter(color);
-  };
-
+  const { workColor, breakColor } = UseSettings();
   const workPallet = ColorUtils.generateColorPalette(workColor);
   const breakPallet = ColorUtils.generateColorPalette(breakColor);
 
   return {
     workColor,
     breakColor,
-    updateWorkColor: updateColor(setWorkColor),
-    updateBreakColor: updateColor(setBreakColor),
-    workPallet,
     breakPallet,
+    workPallet,
     currentPallet: isBreak ? breakPallet : workPallet,
   };
 }

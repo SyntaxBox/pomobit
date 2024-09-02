06178190877c7604cb6de21fd44928ca8f0f99af
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-02 18:31:51 +0100

    fix color selection issues

--- src/routes/settings-page.tsx
+++ src/routes/settings-page.tsx
@@ -1,25 +1,67 @@
 import { useUI } from "../hooks";
-import { Container, Hue, Title } from "../ui";
+import { DEFAULT_SETTINGS, useSettings } from "../hooks/useSettings";
+import { Button, Container, H3, Hue, Title } from "../ui";
 export default function SettingsPage() {
-  const { currentPallet } = useUI();
+  const { currentPallet, workPallet, breakPallet } = useUI();
+  const { workHue, breakHue, updateSettings } = useSettings();
   return (
     <section
       style={{
         background: currentPallet.background + "b2",
+        color: currentPallet.text1,
       }}
       className="flex-grow"
     >
       <Container as="div">
         <div
-          className="rounded-lg p-2"
+          className="rounded-lg p-4"
           style={{
             border: `dashed 1px ${currentPallet.text1}`,
           }}
         >
-          <Title className="p-4">Settings</Title>
+          <Title className="">Settings</Title>
           <div>
-            <h2>Work Color</h2>
-            <Hue hue={120} onChange={() => {}} />
+            <H3>Work Color:</H3>
+
+            <div className="flex gap-3 items-center">
+              <Hue
+                hue={workHue}
+                onChange={(newHsv) => updateSettings({ workHue: newHsv.h })}
+                className="w-full"
+              />
+              <Button
+                pallet={workPallet}
+                className="p-2"
+                onClick={() =>
+                  updateSettings({
+                    workHue: DEFAULT_SETTINGS.workHue,
+                  })
+                }
+              >
+                Reset
+              </Button>
+            </div>
+          </div>
+          <div>
+            <H3>Break Color:</H3>
+            <div className="flex gap-3 items-center h-fit">
+              <Hue
+                hue={breakHue}
+                className="w-full"
+                onChange={(newHsv) => updateSettings({ breakHue: newHsv.h })}
+              />
+              <Button
+                pallet={breakPallet}
+                className="p-2"
+                onClick={() =>
+                  updateSettings({
+                    breakHue: DEFAULT_SETTINGS.breakHue,
+                  })
+                }
+              >
+                Reset
+              </Button>
+            </div>
           </div>
         </div>
       </Container>

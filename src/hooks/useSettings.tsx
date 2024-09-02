4c8d0ccb95902473ff4d44a4ed80d6d2f98829ce
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-02 18:32:38 +0100

    use zustand for global state

--- src/hooks/useSettings.tsx
+++ src/hooks/useSettings.tsx
@@ -1,16 +1,45 @@
+import { useCallback, useEffect } from "react";
+import { useSettingsStore } from "../store";
 import { useLocalStorage } from "./useLocalStorage";
 
-const DEFAULT_WORK = "#ee000e";
-const DEFAULT_BREAK = "#00e030";
+const DEFAULT_WORK_HUE = 99;
+const DEFAULT_BREAK_HUE = 37;
 export const DEFAULT_SETTINGS = {
-  workColor: DEFAULT_WORK,
-  breakColor: DEFAULT_BREAK,
+  workHue: DEFAULT_WORK_HUE,
+  breakHue: DEFAULT_BREAK_HUE,
 };
 
-export function UseSettings() {
-  const [settings, setSettings] = useLocalStorage("settings", DEFAULT_SETTINGS);
-  const updateSettings = (updates: Partial<typeof settings>) => {
-    setSettings({ ...settings, ...updates });
+export function useSettings() {
+  const { workHue, breakHue, setWorkHue, setBreakHue } = useSettingsStore();
+  const [localSettings, setLocalSettings] = useLocalStorage(
+    "settings",
+    DEFAULT_SETTINGS,
+  );
+
+  useEffect(() => {
+    if (workHue === undefined || breakHue === undefined) {
+      setWorkHue(localSettings.workHue);
+      setBreakHue(localSettings.breakHue);
+    }
+  }, []);
+
+  const updateSettings = useCallback(
+    (updates: Partial<typeof localSettings>) => {
+      setLocalSettings({ ...localSettings, ...updates });
+      if (updates.workHue !== undefined) {
+        setWorkHue(updates.workHue);
+      }
+      if (updates.breakHue !== undefined) {
+        setBreakHue(updates.breakHue);
+      }
+    },
+
+    [localSettings, setLocalSettings],
+  );
+  return {
+    workHue: workHue ?? DEFAULT_WORK_HUE,
+    breakHue: breakHue ?? DEFAULT_BREAK_HUE,
+    localSettings,
+    updateSettings,
   };
-  return { ...settings, settings, updateSettings };
 }

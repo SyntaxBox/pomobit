e67e1c1e69f65af93a76f90f4950a63ac84be559
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-07 19:18:55 +0100

    add mintues and seconds transform to seconds

--- src/lib/utils/time.ts
+++ src/lib/utils/time.ts
@@ -3,3 +3,19 @@ export function getMinutesAndSeconds(totalSeconds: number) {
   const seconds = totalSeconds % 60;
   return { minutes, seconds } as const;
 }
+
+export function getSeconds({
+  time,
+  minutes,
+  seconds,
+}: {
+  time: number;
+  minutes?: number;
+  seconds?: number;
+}) {
+  if (minutes === undefined && seconds === undefined) return time;
+  if (minutes === undefined) return time + (seconds || 0);
+  if (seconds === undefined)
+    return minutes * 60 + getMinutesAndSeconds(time).seconds;
+  return minutes * 60 + seconds;
+}

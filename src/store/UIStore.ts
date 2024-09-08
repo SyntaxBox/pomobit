c82276f952403a5c2f99bc7ade1f36c530c7721f
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-08 20:18:56 +0100

    Update UIStore.ts

--- src/store/UIStore.ts
+++ src/store/UIStore.ts
@@ -1,28 +1,17 @@
 import { create } from "zustand";
 
-export const DEFAULT_WORK_BG = "#00e030";
-export const DEFAULT_BREAK_BG = "#ee000e";
-
-interface UIState {
-  workBg: string;
-  breakBg: string;
-  setWorkBg: (bg: string) => void;
-  setBreakBg: (bg: string) => void;
+export interface UIState {
+  workHue?: number;
+  breakHue?: number;
+  setWorkHue: (hue: number) => void;
+  setBreakHue: (hue: number) => void;
 }
 
 export const useUIStore = create<UIState>((set) => ({
-  workBg: DEFAULT_WORK_BG,
-  breakBg: DEFAULT_BREAK_BG,
-  setWorkBg: (bg) => {
-    set((state) => ({
-      ...state,
-      workBg: bg,
-    }));
+  setWorkHue: (hue) => {
+    set((state) => ({ ...state, workHue: hue }));
   },
-  setBreakBg: (bg) => {
-    set((state) => ({
-      ...state,
-      breakBg: bg,
-    }));
+  setBreakHue: (hue) => {
+    set((state) => ({ ...state, breakHue: hue }));
   },
 }));

ca2e3c13f52988690fe035a6a0ade2fa599b9540
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-14 21:04:51 +0100

    add use vision health hook call

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components";
+import { useVisionHealth } from "../hooks";
 
 export default function Root() {
+  useVisionHealth();
   return (
     <main className="min-h-screen flex flex-col">
       <Navbar />

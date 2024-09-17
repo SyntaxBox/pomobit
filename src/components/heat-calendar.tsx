2ebd7e099f85705dfba59723e57c45d840310d97
Abdelhamid Boudjit <qhamid@proton.me>
2024-09-17 15:50:18 +0100

    create heat calendar component

---
+++ src/components/heat-calendar.tsx
@@ -0,0 +1,95 @@
+import { useState, MouseEvent } from "react";
+import HeatMap, { HeatMapValue } from "@uiw/react-heat-map";
+import { ColorUtils } from "../lib/utils";
+
+// Define the structure of the tooltip data
+interface TooltipState {
+  visible: boolean;
+  data: HeatMapValue | null;
+  x: number;
+  y: number;
+}
+
+export function HeatCalendar({
+  pallet,
+  data,
+}: {
+  pallet: ColorUtils.ColorPallet;
+  data: HeatMapValue[];
+}) {
+  const [tooltip, setTooltip] = useState<TooltipState>({
+    visible: false,
+    data: null,
+    x: 0,
+    y: 0,
+  });
+
+  const handleMouseEnter = (
+    e: MouseEvent<SVGRectElement>,
+    data: HeatMapValue,
+  ) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    setTooltip({
+      visible: true,
+      data,
+      x: rect.left + window.scrollX + rect.width / 2,
+      y: rect.top + window.scrollY - 10,
+    });
+  };
+
+  const handleMouseLeave = () => {
+    setTooltip({ visible: false, data: null, x: 0, y: 0 });
+  };
+
+  const shadesRecord = Object.fromEntries(
+    ColorUtils.generateShades(pallet.original, 10).map((c, i) => [3 * i, c]),
+  );
+  return (
+    <div
+      className="relative mx-auto flex-grow"
+      style={{ background: pallet.background + "90" }}
+    >
+      <HeatMap
+        value={data}
+        legendCellSize={0}
+        width={1200}
+        rectSize={20}
+        panelColors={shadesRecord}
+        startDate={new Date(Date.now() - 365 * 24 * 60 * 60 * 1000)}
+        endDate={new Date(Date.now())}
+        rectProps={{
+          rx: 5,
+        }}
+        rectRender={(props, data) => {
+          return (
+            <rect
+              {...props}
+              onMouseEnter={(e) => handleMouseEnter(e, data)}
+              onMouseLeave={handleMouseLeave}
+              style={{ cursor: "pointer" }}
+            />
+          );
+        }}
+      />
+      {tooltip.visible && (
+        <div
+          style={{
+            position: "absolute",
+            left: tooltip.x,
+            top: tooltip.y,
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
+            color: "white",
+            padding: "5px 10px",
+            borderRadius: "4px",
+            fontSize: "12px",
+            pointerEvents: "none",
+            transform: "translate(-50%, -100%)",
+            whiteSpace: "nowrap",
+          }}
+        >
+          {tooltip.data?.date}: {tooltip.data?.count || 0} Work Shifts
+        </div>
+      )}
+    </div>
+  );
+}
